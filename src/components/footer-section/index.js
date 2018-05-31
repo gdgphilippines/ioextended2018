d892c7d814f35b3842175a2f0c8897a319f3a0fd
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-05-31 12:06:28 +0800

    added footer
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ src/components/footer-section/index.js
@@ -0,0 +1,32 @@
+import { ElementLiteLit, html } from '@littleq/element-lite';
+import { template } from './template.js';
+import style from './style.styl';
+const { HTMLElement, customElements } = window;
+
+class Component extends ElementLiteLit(HTMLElement) {
+  static get is () { return 'footer-section'; }
+
+  constructor () {
+    super();
+    this.__data = {};
+  }
+
+  set social (social) {
+    this.__data['img'] = social;
+    this.invalidate();
+  }
+
+  get social () {
+    return this.__data['social'];
+  }
+
+  render () {
+    return html`<style>${style.toString()}</style>${template(this)}`;
+  }
+}
+
+if (!customElements.get(Component.is)) {
+  customElements.define(Component.is, Component);
+} else {
+  console.warn(`${Component.is} is already defined somewhere. Please check your code.`);
+}
---
+++ src/components/footer-section/template.js
@@ -0,0 +1,19 @@
+import { html } from '@littleq/element-lite';
+
+const template = (self) => html`
+  <footer class="footer">
+    <img class="logo" src="/assets/images/gdgph.png">
+    <div class="spacer"></div>
+    <nav class="social-nav">
+      <ul class="social-nav-list">
+        <li class="social-nav">
+          <a class="social-icon-anchor" href="">
+
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+`;
+
+export { template };
