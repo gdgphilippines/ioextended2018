65d475479fa89cb632b778241c070be84345b82d
jkga <johnkennethgibasabella@gmail.com>
2018-06-15 14:49:53 +0800

    Fixed progress-bar issue in home page

--- src/pages/page-home/index.js
+++ src/pages/page-home/index.js
@@ -1,4 +1,5 @@
 import { ElementLiteLit, html, prepareShadyCSS } from '@littleq/element-lite/element-lite-lit.js';
+import { updateState } from '../../utils/ui-state.js';
 import { PageMixin } from '../../mixins/page-mixin/index.js';
 import { template } from './template.js';
 import style from './style.styl';
@@ -47,6 +48,7 @@ class Page extends PageMixin(ElementLiteLit(HTMLElement, style.toString())) {
   }
 
   get about () {
+    updateState('currentProgress', 'loaded');
     return this.__data['about'];
   }
 
@@ -56,6 +58,7 @@ class Page extends PageMixin(ElementLiteLit(HTMLElement, style.toString())) {
   }
 
   get landing () {
+    updateState('currentProgress', 'loaded');
     return this.__data['landing'];
   }
 
