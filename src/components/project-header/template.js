904b71045369593db4a34c23d1d7db741663490e
Andresito de Guzman <andresitomyemail@gmail.com>
2019-07-12 19:59:59 +0800

    Hide navbar and sidebar links

--- src/components/project-header/template.js
+++ src/components/project-header/template.js
@@ -3,11 +3,11 @@ import { html } from '@littleq/element-lite/lib/lit-html/lib/lit-extended.js';
 const template = (self) => html`
   <progress-bar class="header-progress-bar"></progress-bar>
   <header class="header">
-    <button class="button mobile-only" on-click=${self.openSidebar} aria-label="Open Sidebar">
+    <!--button class="button mobile-only" on-click=${self.openSidebar} aria-label="Open Sidebar">
       <svg class="icon" viewBox="0 0 24 24">
         <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
       </svg>
-    </button>
+    </button-->
     <h1 class="title">
       <a class="logo-anchor" href="/" aria-label="Go to I/O 2019 Philippines Homepage">
         <img class="logo" alt="I/O 19 Logo" src="/assets/images/io19.svg">
@@ -16,8 +16,8 @@ const template = (self) => html`
     <div class="spacer">
     </div>
     <navigation-loader>
-      <header-navigation class="not-mobile">
-      </header-navigation>
+      <!--header-navigation class="not-mobile">
+      </header-navigation-->
     </navigation-loader>
   </header>
 `;
